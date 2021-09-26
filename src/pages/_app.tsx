9aab4d311f1a5e4d51282b469ddf557b44eeba51
Daniel Barreto <munizbarreto5@gmail.com>
2021-09-26 12:23:37 -0300

    chore: add eslint and prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,12 @@ import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp ({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
-    
+
   )
 }
 
